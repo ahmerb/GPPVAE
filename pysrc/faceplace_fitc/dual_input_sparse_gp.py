c8d65863e689e1a3091e36dcee339423f475e0f1
Ahmer Butt <ab15015@bristol.ac.uk>
2019-04-01 20:28:49 +0100

    flake8 linting

--- pysrc/faceplace_fitc/dual_input_sparse_gp.py
+++ pysrc/faceplace_fitc/dual_input_sparse_gp.py
@@ -1,21 +1,23 @@
 import torch
 from torch.nn import Parameter
 import torch.distributions as dists
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d  # noqa: F401
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 import matplotlib.pyplot as pl
 import numpy as np
 from sparse_gp import SparseGPRegression
-from kernels import RotationKernel, LinearKernel
+from kernels import RotationKernel
+
 
 class KernelComposer():
     @staticmethod
     def Product(K1, K2):
-        return K1 * K2 # elem-wise
+        return K1 * K2  # elem-wise
 
     @staticmethod
     def Addition(K1, K2):
         return K1 + K2
+
 
 class DualInputSparseGPRegression(SparseGPRegression):
     def __init__(self, X, W, y, x_kernel, w_kernel, kernel_composer, Xu, Wu, mean_function=None, noise=0.5):
@@ -82,13 +84,14 @@ class DualInputSparseGPRegression(SparseGPRegression):
                              .format(self.W.shape[1:], Wnew.shape[1:]))
 
     def _zero_mean_function(self, x, w):
-        N = x.shape[0] # num inputs, also equals w.shape[0]
-        L = self.y.shape[1] # output dimension
+        N = x.shape[0]  # num inputs, also equals w.shape[0]
+        L = self.y.shape[1]  # output dimension
         return x.new_zeros(N, L)
 
     def posterior_predictive(self, Xnew, Wnew, full_cov=False, noiseless=True):
         test_points = (Xnew, Wnew)
-        return super(DualInputSparseGPRegression, self).posterior_predictive(test_points, full_cov=full_cov, noiseless=noiseless)
+        return super(DualInputSparseGPRegression, self) \
+            .posterior_predictive(test_points, full_cov=full_cov, noiseless=noiseless)
 
     def predict_and_plot(self, Xnew, Wnew, nsamples=3):
         N = Xnew.shape[0]
@@ -99,7 +102,7 @@ class DualInputSparseGPRegression(SparseGPRegression):
 
         # draw samples from posterior
         f_posterior = (mu + L.mm(torch.normal(mean=torch.zeros(nsamples, N), std=torch.ones(nsamples, N)).t()).t())
-        
+
         # convert to numpy
         numpy = {
             'X': self.X.detach().numpy(),
@@ -116,24 +119,28 @@ class DualInputSparseGPRegression(SparseGPRegression):
 
         fig = pl.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(numpy['X'], numpy['W'], numpy['y'], c='b')#, 'bs', ms=8)
+        ax.scatter(numpy['X'], numpy['W'], numpy['y'], c='b')  # , 'bs', ms=8)
         for f_post in list(numpy['f_posterior']):
             ax.plot(numpy['Xnew'], numpy['Wnew'], f_post)
-        ax.plot(numpy['Xnew'], numpy['Wnew'], numpy['mu']-2*numpy['Ldiag'], color='#dddddd')
-        ax.plot(numpy['Xnew'], numpy['Wnew'], numpy['mu']+2*numpy['Ldiag'], color='#dddddd')
+        ax.plot(numpy['Xnew'], numpy['Wnew'], numpy['mu']-2*numpy['Ldiag'], color='#dddddd')  # noqa E226
+        ax.plot(numpy['Xnew'], numpy['Wnew'], numpy['mu']+2*numpy['Ldiag'], color='#dddddd')  # noqa E226
         ax.plot(numpy['Xnew'], numpy['Wnew'], numpy['mu'], color='r')
-        ax.scatter(numpy['Xu'], np.zeros(numpy['Xu'].shape[-1]) - ax.get_ylim()[0], np.zeros(numpy['Xu'].shape[-1]) - ax.get_zlim()[0], c='r', marker='^')
-        ax.scatter(np.zeros(numpy['Xu'].shape[-1]) - ax.get_xlim()[0], numpy['Wu'], np.zeros(numpy['Xu'].shape[-1]) - ax.get_zlim()[0], c='g', marker='^')
+        ax.scatter(numpy['Xu'],
+                   np.zeros(numpy['Xu'].shape[-1]) - ax.get_ylim()[0],
+                   np.zeros(numpy['Xu'].shape[-1]) - ax.get_zlim()[0], c='r', marker='^')
+        ax.scatter(np.zeros(numpy['Xu'].shape[-1]) - ax.get_xlim()[0],
+                   numpy['Wu'], np.zeros(numpy['Xu'].shape[-1]) - ax.get_zlim()[0], c='g', marker='^')
         ax.set_title('{} Samples from GP Posterior'.format(nsamples))
         pl.show()
 
+
 def testStuff():
-    N = 100 # num samples
-    M = 7   # num inducing points
+    N = 100  # num samples
+    M = 7    # num inducing points
 
     X = dists.Uniform(0.0, 5.0).sample(sample_shape=(N,))
     W = dists.Uniform(-5.0, 11).sample(sample_shape=(N,))
-    y = 0.5 * torch.sin(3*X) + torch.cos(2*W) + dists.Normal(0.0, 0.2).sample(sample_shape=(N,))
+    y = 0.5 * torch.sin(3 * X) + torch.cos(2 * W) + dists.Normal(0.0, 0.2).sample(sample_shape=(N,))
     Xu = torch.linspace(0.1, 4.9, M)
     Wu = torch.linspace(-4.9, 10.9, M)
 
@@ -156,10 +163,11 @@ def testStuff():
     Wtest = torch.linspace(-5.0, 11, 10)
     sgpr.predict_and_plot(Xtest, Wtest)
 
+
 def testStuff2():
     # multiple-dimension input and output (redefine D as Xdim and define O as num inducing points)
-    N = 100 # num samples
-    O = 7   # num inducing points
+    N = 100  # num samples
+    O = 7    # num inducing points # noqa: E741
 
     D = 10  # X dim
     R = 4   # W dim
@@ -169,11 +177,14 @@ def testStuff2():
     X = dists.Uniform(-10.0, 10.0).sample(sample_shape=(N, D))
     W = dists.Uniform(-20.0,  7.0).sample(sample_shape=(N, R))
     # just do some random matmuls with ones to make the matrix shapes work
-    Y = torch.sin(3*X).t().mm(torch.cos(2*W)).mm(torch.ones(R, N)).t().mm(torch.ones(D, L)) + dists.Normal(0.0, 0.3).sample(sample_shape=(N, L))
+    Y = torch \
+        .sin(3 * X).t().mm(torch.cos(2 * W)) \
+        .mm(torch.ones(R, N)).t().mm(torch.ones(D, L)) + dists.Normal(0.0, 0.3) \
+        .sample(sample_shape=(N, L))
 
     # init inducing points
-    Xu = torch.linspace(-9.99, 9.99, O).expand(D, O).t() # OxD
-    Wu = torch.linspace(-19.9, 6.99, O).expand(R, O).t() # OxR
+    Xu = torch.linspace(-9.99, 9.99, O).expand(D, O).t()  # OxD
+    Wu = torch.linspace(-19.9, 6.99, O).expand(R, O).t()  # OxR
 
     sgpr = DualInputSparseGPRegression(X, W, Y, RotationKernel, RotationKernel, KernelComposer.Product, Xu, Wu)
     optimizer = torch.optim.Adam(sgpr.parameters(), lr=0.005)
@@ -194,7 +205,7 @@ def testStuff2():
     Ntest = 5
     Xtest = dists.Uniform(-10.0, 10.0).sample(sample_shape=(Ntest, D))
     Wtest = dists.Uniform(-20.0,  7.0).sample(sample_shape=(Ntest, R))
-    Ftest = torch.sin(3*Xtest).t().mm(torch.cos(2*Wtest)).mm(torch.ones(R, Ntest)).t().mm(torch.ones(D, L))
+    Ftest = torch.sin(3 * Xtest).t().mm(torch.cos(2 * Wtest)).mm(torch.ones(R, Ntest)).t().mm(torch.ones(D, L))
 
     Fpred = sgpr.posterior_predictive(Xtest, Wtest)
     print("MSE = ", torch.dist(Ftest, Fpred))
